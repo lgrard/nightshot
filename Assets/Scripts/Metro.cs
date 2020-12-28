9bb698a87fc6cd54923d864338cd394e522f19bc
DilaneP <d.petrovic@student.isartdigital.com>
2020-12-29 00:07:14 +0100

    Metro spawner and metro movement + damage

---
+++ Assets/Scripts/Metro.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Metro : MonoBehaviour
+{
+    [SerializeField, Range(0f, 50f)] private float speed = 20f;
+    [SerializeField, Range(0f, 50f)] private float lifeTime = 10f;
+    [SerializeField] int damage = 10;
+
+    private float       timer = 0.0f;
+    private Rigidbody   rb;
+
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        
+
+        rb.velocity = transform.right * speed;
+        timer += Time.deltaTime;
+
+        if (timer >= lifeTime)
+        {
+            Destroy(gameObject);
+        }        
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+
+        collision.gameObject.TryGetComponent<PlayerController>(out PlayerController playerController);
+
+        if(playerController != null)
+        {
+            playerController.TakeDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject obj;
+    [SerializeField] float waitingTime = 10f;
+    private float wait;
+
+    void Start()
+    {
+        wait = waitingTime;
+    }
+
+    
+    void Update()
+    {
+        if (waitingTime > 0)
+        {
+            waitingTime -= Time.deltaTime;
+        }
+        else if (waitingTime <= 0)
+        {
+            waitingTime = wait;
+            GameObject metro = Instantiate(obj, gameObject.transform.position, gameObject.transform.rotation,transform.parent);
+            
+        }
+    }
+}
